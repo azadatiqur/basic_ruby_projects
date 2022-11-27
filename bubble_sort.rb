6452af57ec3a6ae091dfa18c32b855c502fb6740
azadatiqur <mdatiqurrahman.1010@gmail.com>
2022-11-27 22:07:25 +0600

    Add code for bubble_sort project Problem:     Build a method #bubble_sort that takes an array and returns a sorted     array.
    Parameter:
        An array
    
    Return type:
        An ascending/descending sorted array
    
    Solution:
        compare each element with the next element and swapping if the
        previous element is greater/lower and do this until the array is
        sorted.
    
    Time Complexity:
        Worst case:O(n^2)
    
    Space Complexity:
        O(n)
    
    Used:
        -> ruby array & array methods(.length)
        -> ruby variable
        -> ruby for loop
        -> ruby boolean
        -> ruby conditionals
        -> ruby function
        -> swapping variables
        -> ruby break loop

---
+++ bubble_sort.rb
@@ -0,0 +1,35 @@
+=begin
+
+    compare one element of the array with its succeeding element
+    n-1 times where n is the size of the array;
+    
+    used not_sorted boolean to stop the sorting if all elements are sorted;
+
+    as after m iteration m last elements are sorted that's why loop it to only 
+    n - m range where n is the size of the array;
+
+=end
+
+def bubble_sort(ara) 
+    ara_size = ara.length
+    not_sorted = true
+    for i in 0..(ara_size-2) do
+        if not_sorted == false 
+            break
+        end
+        not_sorted = false
+        for i in 0..(ara_size-i-2) do
+            if ara[i] > ara[i+1] 
+                temp = ara[i]
+                ara[i] = ara[i+1]
+                ara[i+1] = temp
+                not_sorted = true
+            end
+        end 
+    end
+    ara
+end
+
+
+arr = [6,5,4,3,2,1]
+p bubble_sort(arr)
